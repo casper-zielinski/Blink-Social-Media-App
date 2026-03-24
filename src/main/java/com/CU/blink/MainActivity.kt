6a0ca9541c041acb8b41061c88a2a943037c3092
casper-zielinski <casper.zielinski@edu.fh-joanneum.at>
2026-03-24 11:08:19 +0100

    merge: resolved merge confilcts

--- src/main/java/com/CU/blink/MainActivity.kt
+++ src/main/java/com/CU/blink/MainActivity.kt
@@ -15,11 +15,7 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
-<<<<<<< HEAD
-=======
-import androidx.compose.material3.Text
 import androidx.compose.runtime.LaunchedEffect
->>>>>>> 2028c552e9cbccccb3cf6f76f20a73ba3235ef55
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
