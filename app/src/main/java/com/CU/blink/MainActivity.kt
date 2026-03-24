73eb9c7f8a004c9ac0e4e30788403e11dbb60736
casper-zielinski <casper.zielinski@edu.fh-joanneum.at>
2026-03-24 10:55:16 +0100

    feat: add Theming to MainActivity

--- app/src/main/java/com/CU/blink/MainActivity.kt
+++ app/src/main/java/com/CU/blink/MainActivity.kt
@@ -15,7 +15,6 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
